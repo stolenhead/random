5b8dcedaed699ed156a055c62e88df05c36e3499
ANDREA <andrea.palomino.ruby@gmail.com>
2016-09-07 15:16:07 -0700

    new call

--- eje4/eje4.js
+++ eje4/eje4.js
@@ -2,7 +2,9 @@ window.addEventListener("load", function() {
 	var boton = document.getElementById("cambiar");
 	boton.addEventListener("click", function() {
 		var segundos = parseFloat(document.getElementById("segundos").value);
-         setInterval(function () {
-          document.body.style.backgroundColor ='#'+Math.floor(Math.random()*16777215).toString(16);;}, segundos*1000);
+         setInterval(callback,segundos*1000);
     });
 });
+function callback () {
+          document.body.style.backgroundColor ='#'+Math.floor(Math.random()*16777215).toString(16);
+}
